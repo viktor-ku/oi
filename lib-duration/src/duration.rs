bc80d26069a241c9d8a7237f7852822e3169ce33
Viktor Kuroljov <viktor@ku.family>
2020-09-01 20:21:11 +0300

    lib-duration: impl secs method

--- lib-duration/src/duration.rs
+++ lib-duration/src/duration.rs
@@ -2,18 +2,22 @@ use crate::RawDuration;
 
 #[derive(Debug, PartialEq)]
 pub struct Duration {
-    pub hours: u32,
-    pub minutes: u32,
-    pub seconds: u32,
+    pub hours: u64,
+    pub minutes: u64,
+    pub seconds: u64,
 }
 
 impl Duration {
-    pub fn new(hours: u32, minutes: u32, seconds: u32) -> Self {
+    pub fn new(hours: u64, minutes: u64, seconds: u64) -> Self {
         Self {
             hours,
             minutes,
             seconds,
         }
+    }
+
+    pub fn secs(&self) -> u64 {
+        self.seconds + (self.minutes * 60) + (self.hours * 3600)
     }
 
     fn fix_overflow(&mut self) {
@@ -32,15 +36,15 @@ impl Duration {
         for raw_duration in raw {
             match raw_duration {
                 RawDuration::Hours(hours) => {
-                    it.hours += *hours as u32;
-                    it.minutes += (hours.fract() * 60.0) as u32;
+                    it.hours += *hours as u64;
+                    it.minutes += (hours.fract() * 60.0) as u64;
                 }
                 RawDuration::Minutes(minutes) => {
-                    it.minutes += *minutes as u32;
-                    it.seconds += (minutes.fract() * 60.0) as u32;
+                    it.minutes += *minutes as u64;
+                    it.seconds += (minutes.fract() * 60.0) as u64;
                 }
                 RawDuration::Seconds(seconds) => {
-                    it.seconds += *seconds as u32;
+                    it.seconds += *seconds as u64;
                 }
             }
         }
