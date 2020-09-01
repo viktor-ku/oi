8cb3c89f864d87f2f0d6fb4a65166985791c00e9
Viktor Kuroljov <viktor@ku.family>
2020-09-01 16:57:28 +0300

    lib-duration add no_duration tests

---
+++ lib-duration/tests/no_duration.rs
@@ -0,0 +1,16 @@
+use lib_duration::{duration, Duration};
+use pretty_assertions::assert_eq;
+
+#[test]
+fn one_word() {
+    let input = "yo";
+    let actual = duration(input);
+    assert_eq!(actual.unwrap(), Duration::default());
+}
+
+#[test]
+fn words() {
+    let input = "one two three no durations can you see";
+    let actual = duration(input);
+    assert_eq!(actual.unwrap(), Duration::default());
+}
