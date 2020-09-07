c5765b6f649bc5427a9679461a369bef9898fdd8
Viktor Kuroljov <viktor@ku.family>
2020-09-08 01:33:37 +0300

    kind of using single pest file thank you

--- lib-duration/tests/at.rs
+++ lib-duration/tests/at.rs
@@ -6,18 +6,16 @@ mod with_part {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn test_pm_from_1_to_11() {
-        let pm_values: &[u64] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    fn at_1pm() {
+        let input = "at 1pm";
+        let now = Utc.ymd(2020, 9, 1).and_hms(12, 30, 0);
+        assert_eq!(duration(input, &now), Ok(Duration::new(0, 30, 0)));
+    }
 
-        for pm in pm_values {
-            let input = &format!("at {}pm", pm);
-            let now = Utc.ymd(2020, 9, 1).and_hms(12, 0, 0);
-            let actual = duration(input, &now).unwrap();
-            let expected = Duration::new(*pm, 0, 0);
-            if actual != expected {
-                println!("{} failed", input);
-            }
-            assert_eq!(actual, Duration::new(*pm, 0, 0));
-        }
+    #[test]
+    fn at_2pm() {
+        let input = "at 2pm";
+        let now = Utc.ymd(2020, 9, 1).and_hms(12, 30, 0);
+        assert_eq!(duration(input, &now), Ok(Duration::new(1, 30, 0)));
     }
 }
