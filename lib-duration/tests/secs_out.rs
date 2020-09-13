f11d34de046f45a5facd1d7c5b768cd23de631e8
Viktor Kuroljov <viktor@ku.family>
2020-09-13 17:12:23 +0300

    use Local instead of Utc

--- lib-duration/tests/secs_out.rs
+++ lib-duration/tests/secs_out.rs
@@ -1,38 +1,38 @@
-use chrono::Utc;
+use chrono::Local;
 use lib_duration::duration;
 use pretty_assertions::assert_eq;
 
 #[test]
 fn a01() {
     let input = "1 sec";
-    let d = duration(input, &Utc::now()).unwrap();
+    let d = duration(input, &Local::now()).unwrap();
     assert_eq!(d.secs(), 1);
 }
 
 #[test]
 fn a02() {
     let input = "1 min";
-    let d = duration(input, &Utc::now()).unwrap();
+    let d = duration(input, &Local::now()).unwrap();
     assert_eq!(d.secs(), 60);
 }
 
 #[test]
 fn a03() {
     let input = "1 hour";
-    let d = duration(input, &Utc::now()).unwrap();
+    let d = duration(input, &Local::now()).unwrap();
     assert_eq!(d.secs(), 3600);
 }
 
 #[test]
 fn a04() {
     let input = "1h 1m 1s";
-    let d = duration(input, &Utc::now()).unwrap();
+    let d = duration(input, &Local::now()).unwrap();
     assert_eq!(d.secs(), 3661);
 }
 
 #[test]
 fn a05() {
     let input = "25m 330 seconds";
-    let d = duration(input, &Utc::now()).unwrap();
+    let d = duration(input, &Local::now()).unwrap();
     assert_eq!(d.secs(), 1830);
 }
