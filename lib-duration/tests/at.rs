86eb707a7dc42e5072d0f147cb378150fe8ed361
Viktor Kuroljov <viktor@ku.family>
2020-09-13 23:47:28 +0300

    Revert "support 24h format"
    This reverts commit 46f30546c63ab16d9f0fa83e13a0047aeafbbb8b.

--- lib-duration/tests/at.rs
+++ lib-duration/tests/at.rs
@@ -1,15 +1,15 @@
 use chrono::{Local, TimeZone};
 use lib_duration::{duration, Duration};
 
-mod h12_format {
+mod with_part {
     use super::*;
     use pretty_assertions::assert_eq;
 
     #[test]
     fn long_variant_should_work_too() {
-        let input = "at 1 am";
-        let now = Local.ymd(2020, 9, 30).and_hms(0, 39, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(0, 21, 0)));
+        let input = "at 0 am";
+        let now = Local.ymd(2020, 9, 30).and_hms(23, 45, 0);
+        assert_eq!(duration(input, &now), Ok(Duration::new(0, 15, 0)));
     }
 
     #[test]
@@ -192,171 +192,5 @@ mod h12_format {
         let input = "at 12am";
         let now = Local.ymd(2020, 9, 30).and_hms(23, 45, 0);
         assert_eq!(duration(input, &now), Ok(Duration::new(0, 15, 0)));
-    }
-}
-
-mod h24_format {
-    use super::*;
-    use pretty_assertions::assert_eq;
-
-    #[test]
-    fn at_1() {
-        let input = "at 1";
-        let now = Local.ymd(2020, 9, 30).and_hms(0, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(0, 15, 0)));
-    }
-
-    #[test]
-    fn at_2() {
-        let input = "at 2";
-        let now = Local.ymd(2020, 9, 30).and_hms(0, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(1, 15, 0)));
-    }
-
-    #[test]
-    fn at_3() {
-        let input = "at 3";
-        let now = Local.ymd(2020, 9, 30).and_hms(0, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(2, 15, 0)));
-    }
-
-    #[test]
-    fn at_4() {
-        let input = "at 4";
-        let now = Local.ymd(2020, 9, 30).and_hms(0, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(3, 15, 0)));
-    }
-
-    #[test]
-    fn at_5() {
-        let input = "at 5";
-        let now = Local.ymd(2020, 9, 30).and_hms(0, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(4, 15, 0)));
-    }
-
-    #[test]
-    fn at_6() {
-        let input = "at 6";
-        let now = Local.ymd(2020, 9, 30).and_hms(0, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(5, 15, 0)));
-    }
-
-    #[test]
-    fn at_7() {
-        let input = "at 7";
-        let now = Local.ymd(2020, 9, 30).and_hms(5, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(1, 15, 0)));
-    }
-
-    #[test]
-    fn at_8() {
-        let input = "at 8";
-        let now = Local.ymd(2020, 9, 30).and_hms(5, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(2, 15, 0)));
-    }
-
-    #[test]
-    fn at_9() {
-        let input = "at 9";
-        let now = Local.ymd(2020, 9, 30).and_hms(5, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(3, 15, 0)));
-    }
-
-    #[test]
-    fn at_10() {
-        let input = "at 10";
-        let now = Local.ymd(2020, 9, 30).and_hms(5, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(4, 15, 0)));
-    }
-
-    #[test]
-    fn at_11() {
-        let input = "at 11";
-        let now = Local.ymd(2020, 9, 30).and_hms(5, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(5, 15, 0)));
-    }
-
-    #[test]
-    fn at_12() {
-        let input = "at 12";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(2, 15, 0)));
-    }
-
-    #[test]
-    fn at_13() {
-        let input = "at 13";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(3, 15, 0)));
-    }
-
-    #[test]
-    fn at_14() {
-        let input = "at 14";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(4, 15, 0)));
-    }
-
-    #[test]
-    fn at_15() {
-        let input = "at 15";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(5, 15, 0)));
-    }
-
-    #[test]
-    fn at_16() {
-        let input = "at 16";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(6, 15, 0)));
-    }
-
-    #[test]
-    fn at_17() {
-        let input = "at 17";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(7, 15, 0)));
-    }
-
-    #[test]
-    fn at_18() {
-        let input = "at 18";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(8, 15, 0)));
-    }
-
-    #[test]
-    fn at_19() {
-        let input = "at 19";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(9, 15, 0)));
-    }
-
-    #[test]
-    fn at_20() {
-        let input = "at 20";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(10, 15, 0)));
-    }
-
-    #[test]
-    fn at_21() {
-        let input = "at 21";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(11, 15, 0)));
-    }
-
-    #[test]
-    fn at_22() {
-        let input = "at 22";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(12, 15, 0)));
-    }
-
-    #[test]
-    fn at_23() {
-        let input = "at 23";
-        let now = Local.ymd(2020, 9, 30).and_hms(9, 45, 0);
-        assert_eq!(duration(input, &now), Ok(Duration::new(13, 15, 0)));
     }
 }
