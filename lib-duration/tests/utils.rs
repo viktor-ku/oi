31365c7d5f2832081ab3f3d8b8e2f23fe6852961
Viktor Kuroljov <viktor@ku.family>
2020-09-13 20:04:50 +0300

    add support for next day overflows

---
+++ lib-duration/tests/utils.rs
@@ -0,0 +1,12 @@
+#[macro_export]
+macro_rules! time {
+    ($h:literal:$m:literal:$s:literal) => {{
+        use chrono::{Local, TimeZone};
+        Local.ymd(2020, 9, 30).and_hms($h, $m, $s)
+    }};
+
+    ($h:literal:$m:literal) => {{
+        use chrono::{Local, TimeZone};
+        Local.ymd(2020, 9, 30).and_hms($h, $m, 0)
+    }};
+}
