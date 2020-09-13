f6271067bfe84ddbdeaa19cd20d1bd4929b5279f
Viktor Kuroljov <viktor@ku.family>
2020-09-13 23:57:20 +0300

    feat: add support for timers based on exact time (#3)

--- lib-duration/src/lib.rs
+++ lib-duration/src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate pest_derive;
 
+use chrono::{DateTime, Local};
 use pest::Parser;
 
 #[derive(Parser)]
@@ -16,13 +17,20 @@ use error::Error;
 mod raw_duration;
 use raw_duration::RawDuration;
 
-pub fn duration(input: &str) -> Result<Duration, Error> {
+mod at_time;
+use at_time::AtTime;
+
+pub fn duration(input: &str, now: &DateTime<Local>) -> Result<Duration, Error> {
     let raw_durations: Vec<RawDuration> = match InputParser::parse(Rule::Input, input) {
         Ok(parsed) => {
             let mut v = Vec::with_capacity(32);
 
             for expr in parsed {
                 match expr.as_rule() {
+                    Rule::AtTime => {
+                        let at = AtTime::new(expr.into_inner());
+                        v.push(RawDuration::Seconds(at.diff(now) as f64));
+                    }
                     Rule::DurationExpr => {
                         let mut needle: f64 = 0.0;
 
@@ -46,7 +54,10 @@ pub fn duration(input: &str) -> Result<Duration, Error> {
 
             v
         }
-        Err(e) => return Err(Error {}),
+        Err(e) => {
+            println!("err {}", e);
+            return Err(Error {});
+        }
     };
 
     Ok(Duration::merge(&raw_durations))
--- lib-duration/tests/optional_fract_part.rs
+++ lib-duration/tests/optional_fract_part.rs
@@ -1,23 +1,24 @@
+use chrono::Local;
 use lib_duration::{duration, Duration};
 use pretty_assertions::assert_eq;
 
 #[test]
 fn a01() {
     let input = "almost 10.h";
-    let actual = duration(input);
+    let actual = duration(input, &Local::now());
     assert_eq!(actual.unwrap(), Duration::new(10, 0, 0));
 }
 
 #[test]
 fn a02() {
     let input = "try 1.h 3600. seconds and 30. mins";
-    let actual = duration(input);
+    let actual = duration(input, &Local::now());
     assert_eq!(actual.unwrap(), Duration::new(2, 30, 0));
 }
 
 #[test]
 fn a03() {
     let input = ".25h 15.m 10. sec";
-    let actual = duration(input);
+    let actual = duration(input, &Local::now());
     assert_eq!(actual.unwrap(), Duration::new(0, 30, 10));
 }
