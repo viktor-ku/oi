c51266705957c8c532f5e22ac136303a8f482ea9
Viktor Kuroljov <viktor@ku.family>
2020-09-25 13:02:59 +0300

    refactor(oid): move app into its own file

--- oid/src/main.rs
+++ oid/src/main.rs
@@ -1,156 +1,18 @@
-use actix_web::{get, post, web, App, HttpServer};
-use lib_api as api;
-use lib_player::Player;
-use lib_store::Store;
-use notify_rust::{Notification, Urgency};
-use std::sync::Mutex;
 use structopt::StructOpt;
-use tokio::sync::mpsc;
-use tokio::task::{spawn, spawn_blocking};
-use tokio::time::{self, Duration};
 
 mod cli;
 use cli::Cli;
 
 mod config;
-use config::Config;
+pub use config::Config;
 
 mod norm_path;
 pub use norm_path::norm_path;
 
 mod on_timeout;
 
-#[derive(Debug)]
-struct ChannelMessage {
-    duration: i64,
-    timer: String,
-}
-
-#[get("/timers/active")]
-async fn find_active_timers(store: web::Data<Store>) -> api::timer::FindActiveTimersResponse {
-    let records = store.records.find_active().await;
-    api::timer::FindActiveTimersResponse {
-        timers: records
-            .iter()
-            .map(|val| api::timer::Timer {
-                start: val.start,
-                duration: val.remaining as u64,
-                message: val.message.clone(),
-            })
-            .collect(),
-    }
-}
-
-#[get("/timers")]
-async fn find_all_timers(store: web::Data<Store>) -> api::timer::FindAllTimersResponse {
-    let records = store.records.find_all().await;
-    api::timer::FindAllTimersResponse {
-        timers: records
-            .iter()
-            .map(|val| api::timer::Timer {
-                start: val.start,
-                duration: val.remaining as u64,
-                message: val.message.clone(),
-            })
-            .collect(),
-    }
-}
-
-#[post("/timer")]
-async fn create_timer(
-    tx: web::Data<Mutex<mpsc::Sender<ChannelMessage>>>,
-    store: web::Data<Store>,
-    payload: web::Json<api::timer::CreateTimer>,
-) -> api::timer::CreateTimerResponse {
-    let id = store
-        .records
-        .create(lib_store::CreateRecord {
-            start: payload.start,
-            message: payload.message.clone(),
-            duration: payload.duration,
-        })
-        .await;
-
-    tx.lock()
-        .unwrap()
-        .send(ChannelMessage {
-            duration: payload.duration as i64,
-            timer: payload.message.clone(),
-        })
-        .await
-        .unwrap();
-
-    api::timer::CreateTimerResponse { uuid: id }
-}
-
-async fn run_timer(duration: u64, timer: String) {
-    println!("gotta sleep for {} secs now, bye...", duration / 1_000);
-    time::delay_for(Duration::from_millis(duration)).await;
-    println!("opacha! time to wake up!");
-
-    spawn_blocking(move || {
-        let config = Config::new();
-
-        Notification::new()
-            .summary("oi")
-            .body(&timer)
-            .timeout(10_000)
-            .urgency(Urgency::Critical)
-            .show()
-            .unwrap();
-
-        if let Some(sound_path) = &config.on_timeout.play {
-            if sound_path.is_file() {
-                let player = Player::new(config.volume());
-                player.play(&sound_path);
-            }
-        }
-    })
-    .await
-    .unwrap();
-}
-
-async fn app(cli: Cli) -> std::io::Result<()> {
-    println!("Sandbox in use: #{}", cli.sandbox);
-
-    let (tx, mut rx) = mpsc::channel::<ChannelMessage>(32);
-    let store = Store::new(Config::config_dir().unwrap(), cli.sandbox).await;
-
-    spawn(async move {
-        println!("Listening for new sleeping requests :)");
-
-        while let Some(msg) = rx.recv().await {
-            spawn(async move {
-                run_timer(msg.duration as u64, msg.timer).await;
-            });
-        }
-    });
-
-    let active_records = store.records.find_active().await;
-    for record in active_records {
-        tx.clone()
-            .send(ChannelMessage {
-                duration: record.remaining,
-                timer: record.message.clone(),
-            })
-            .await
-            .unwrap();
-    }
-
-    println!("Server is up and running, ready for accepting stuff!");
-
-    HttpServer::new(move || {
-        App::new()
-            .data(Mutex::new(tx.clone()))
-            .data(store.clone())
-            .service(create_timer)
-            .service(find_all_timers)
-            .service(find_active_timers)
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
+mod app;
+use app::app;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
